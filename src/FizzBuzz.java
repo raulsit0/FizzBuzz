64ccedbb27f357bd20f6bc88af0ec2fb6a1d0379
raulsit0 <81935488+raulsit0@users.noreply.github.com>
2022-10-18 20:05:46 -0500

    Did not  import correctly so i copy and pasted

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class FizzBuzz {
+class FizzBuzz
+{
+    public static void main(String[] args)
+    {
+//for loop
+        for (int i = 1 ; i <= 100; i++)
+        {
+//check if divisible by both 3 and 5
+            if( i % 3 == 0 && i % 5 == 0)
+            {
+                System.out.println("FizzBuzz");
+            }
+//check if divisible by 3
+            else if(i % 3 ==0)
+            {
+                System.out.println("Fizz");
+            }
+//check if divisible by 5
+            else if(i % 5 ==0)
+            {
+                System.out.println("Buzz");
+            }
+            else
+            {
+                System.out.println(i);
+            }
+        }
+    }
 }
